7239e0a6d3321e6530ed6301ac487d4af9f1136d
William Vo <w.vo467@gmail.com>
2019-12-18 23:54:50 -0800

    Added script to folder

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,10 +9,11 @@ greatest_increase = 0
 greatest_decrease = 0
 
 #Open csv
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "budget_data.csv")
 
-with open(csvpath, newline=" ") as csvfile:
+with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreaderm None)
+    next(csvreader, None)
+    row = next(csvreader)
 
 #Calculate total months and rev
