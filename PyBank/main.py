c2441d7fedb005cab19af4f5372650dc277651f7
William Vo <w.vo467@gmail.com>
2019-12-18 21:27:51 -0800

    Added to homework file PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,18 @@
 import os
 import csv
 
+#Set variables 
+total_months = 0
+total_revenue = 0
+avg_changes = []
+greatest_increase = 0
+greatest_decrease = 0
+
+#Open csv
 csvpath = os.path.join("..", "Resources", "budget_data.csv")
 
 with open(csvpath, newline=" ") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
+    next(csvreaderm None)
+
+#Calculate total months and rev
